2448a73c7f6a7fe25ca30db8501626475050117d
rdrakey <ryan@ryandrake.com>
2013-06-19 19:00:00 +1000

    Minor edit

--- mainlab.rb
+++ mainlab.rb
@@ -5,33 +5,5 @@
 require 'pry'
 require 'pry-debugger'
 
-def listofstudents()
-  studentlist = "Jack Andrew Sanjita Ryan Nick Steve Hui Will Tarun Dan Mike".split
-  randomlist = studentlist.shuffle
-  return randomlist
-end
 
-def calculategroups
-  puts "Please enter group size"
-  groupsize = gets.chomp.to_i
-  number_of_groups = 11 / groupsize
-  remainder = 11 % groupsize
-  if remainder != 0
-    lastgroup = groupsize+remainder
-    neq_groups = number_of_groups - 1
-    return lastgroup, neq_groups,groupsize
-  end
-end
-
-lastgrp, neq, grpsize = calculategroups
-studlist = listofstudents
-finalgroup = []
-while neq != 0
-  newlist = studlist.pop grpsize
-  neq = neq - 1
-  finalgroup.push newlist
-end
-newlist = studlist.pop lastgrp
-finalgroup.push newlist
-puts "#{finalgroup}"
 
