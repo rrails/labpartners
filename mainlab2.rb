0a098d2865522d29e9cad77923fa3c9ff7053f49
RRails <sanjita_quinn@hotmail.com>
2013-06-19 21:26:30 +1000

    Rewrote using array processing

---
+++ mainlab2.rb
@@ -0,0 +1,46 @@
+#Authors
+#Ryan Drake
+#Sanjita Shah
+
+
+def listofstudents()
+  studentlist = "Jack Andrew Sanjita Ryan Nick Steve Hui Will Tarun Dan Mike".split
+  list = studentlist.shuffle
+  return list
+end
+
+def validgroupsize
+  puts "Please enter group size"
+  size = gets.chomp.to_i
+    while size <= 0
+      puts "Please enter valid group size"
+      size = gets.chomp.to_i
+    end
+  return size
+end
+
+randomlist = listofstudents
+grpsize = validgroupsize
+
+fgrp = []
+if randomlist.length > grpsize #class size is greater than group size
+
+  #Split the class into groups of groupsize
+  while !randomlist.empty?
+    newlist = randomlist.pop grpsize
+    fgrp.push newlist
+  end
+
+  if fgrp.last.length < grpsize #Merge the last 2 groups if the last groups is smaller then group size
+      fgrp[fgrp.length-2] = fgrp[fgrp.length-2..fgrp.length-1].flatten
+      fgrp.pop 1
+  end
+
+  i = 0
+  fgrp.each {|fgrp| puts "Group " "#{i=i+1} " "#{fgrp.join(" ")}"}
+
+else #if group size is greater class size
+
+  puts "Group 1 #{randomlist.join(" ")}"
+
+end
